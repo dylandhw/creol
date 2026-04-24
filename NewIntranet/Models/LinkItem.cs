33de5bef5b0dd834de363a5ce61c011ce9f81428
dylandhw <dylanwilkins2308@gmail.com>
2026-04-24 14:54:15 -0400

    card components

---
+++ NewIntranet/Models/LinkItem.cs
@@ -0,0 +1,8 @@
+namespace NewIntranet.Models;
+
+public class LinkItem
+{
+    public string Title { get; set; } = string.Empty;
+    public string Url { get; set; } = string.Empty;
+    public bool IsFavorite { get; set; }
+}
--- NewIntranet/obj/Debug/net10.0/NewIntranet.AssemblyInfo.cs
+++ NewIntranet/obj/Debug/net10.0/NewIntranet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewIntranet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3b22582e49e3d5a717a6e61ab23af8e5ebde1a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b12e6925199eb7f0d1e3a18aa09a53a08c4db651")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewIntranet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewIntranet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
